40fc46eaf4863bfd5e47f0791bb404649e18978b
Linh <lcahu91@gmail.com>
2021-07-23 16:31:29 -0400

    uncommented out audio

--- src/pomodoro/Pomodoro.js
+++ src/pomodoro/Pomodoro.js
@@ -70,7 +70,7 @@ function Pomodoro() {
   useInterval(
     () => {
       if (session.timeRemaining === 0) {
-        // new Audio("https://bigsoundbank.com/UPLOAD/mp3/1482.mp3").play();
+        new Audio("https://bigsoundbank.com/UPLOAD/mp3/1482.mp3").play();
         return setSession(nextSession(focusDuration, breakDuration));
       }
       return setSession(nextTick);
@@ -124,7 +124,7 @@ function Pomodoro() {
 
   const handleBreakIncrease = () => {
     setBreakDuration((currentDuration) => Math.min(15, currentDuration + 1));
-  };
+  }; 
 
   const percentTime =
     (1 -
